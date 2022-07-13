12338687b9b64184003bf1531bd478d8cb522a21
watcharakorn.may <watcharagorn.may@gmail.com>
2022-07-14 02:07:29 +0700

    Feat: Refactor start.sh file

--- start.sh
+++ start.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
-echo "ENVIRONMENT : ${environment}"
-# yes | python manage.py makemigrations
-echo "migration execution..."
 
-python3 -u rogue.py './config/settings/template.py' "$PROJECT_CONFIG" ${environment}
+# exit when any command fails
+set -e
+
+echo "ENVIRONMENT : ${environment}"
 python manage.py create_superuser --username admin --email admin@gmail.com --password admin
 python manage.py migrate
-
-if [ ${environment} = "regressiontest" ]; 
-then
-    python initial.py regressiontest
-fi
-
-if [ $? != 0 ] ; 
-then
-    ispassMigration=false
-    echo "---------------- MANUAL LOG: Migration Fail! ----------------"
-else
-    ispassMigration=true
-    echo "---------------- MANUAL LOG: Migration Succeed! ----------------"
-fi
-
-if [ "$ispassMigration" = "true" ];
-then
-    echo "---------------- MANUAL LOG: Starting Server! ----------------"
-    python manage.py collectstatic --clear --noinput
-    python manage.py runserver 0.0.0.0:8080
-    echo "---------------- MANUAL LOG: StartServer Succeed! ----------------"
-else
-    echo "---------------- MANUAL LOG: Starting Server Fail! ----------------"
-fi
+python manage.py collectstatic --clear --noinput
+python manage.py runserver 0.0.0.0:8080
