34e16633c2bf5b703681911101057203c32f8705
watcharakorn.may <watcharagorn.may@gmail.com>
2022-08-01 02:22:05 +0700

    Feat: update start.sh command order

--- start-dev.sh
+++ start-dev.sh
@@ -4,8 +4,7 @@
 set -e
 
 echo "ENVIRONMENT : ${environment}"
-python3 -u rogue.py './config/settings/template.py' "$PROJECT_CONFIG" ${environment}
-python manage.py create_superuser --username admin --email admin@gmail.com --password admin
 python manage.py migrate
+python manage.py create_superuser --username admin --email admin@gmail.com --password admin
 python manage.py collectstatic --clear --noinput
 python manage.py runserver 0.0.0.0:8080
--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@
 set -e
 
 echo "ENVIRONMENT : ${environment}"
-python manage.py create_superuser --username admin --email admin@gmail.com --password admin
+python3 -u rogue.py './config/settings/template.py' "$PROJECT_CONFIG" ${environment}
 python manage.py migrate
+python manage.py create_superuser --username admin --email admin@gmail.com --password admin
 python manage.py collectstatic --clear --noinput
 python manage.py runserver 0.0.0.0:8080
